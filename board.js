e3dfd39752ee4b17f91429624a92c9a586410a52
Athavan Srividiyakaran <athavan@Athavans-MBP.localdomain>
2020-09-01 13:58:15 +0100

    Latest Progress after Friday

--- board.js
+++ board.js
@@ -1,20 +1,55 @@
 //setupBoard(row, column);
 
-let gameBoardValues = [
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null]
-]
+let gameBoardValues = []
+let currentPlayer = 'red'
 
-function resetBoard() {
+function getGameBoardValues() {
+    return gameBoardValues;
+}
+
+function setGameBoardValues(updatedGameBoardValues) {
+    gameBoardValues = updatedGameBoardValues;
+}
+
+function resetBoard(row, column) {
     document.getElementById("game-board").innerHTML = "";
-    gameBoardValues = [
-        [null, null, null, null, null, null],
-        [null, null, null, null, null, null],
-        [null, null, null, null, null, null],
-        [null, null, null, null, null, null]
-    ]
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < column; j++) {
+            gameBoardValues[row][column] = null;
+        }
+    }
+    document.getElementById("player-counter").style.background = "red"
+}
+
+function setupGameBoardValues(row, column) {
+    let customGameBoardValues = [];
+    for (let i = 0; i < column; i++) {
+        let tempArray = []
+        for (let j = 0; j < row; j++) {
+            tempArray.push(null);
+        }
+        customGameBoardValues.push(tempArray)
+    }
+    gameBoardValues = customGameBoardValues;
+}
+
+function placeCounter(columnIndex) {
+    for (let i = gameBoardValues[columnIndex].length - 1; i >= 0; i--) {
+        if (gameBoardValues[columnIndex][i] == null) {
+            gameBoardValues[columnIndex][i] = "red";
+            let slot = document.getElementById(columnIndex + "," + i)
+            slot.style.background = currentPlayer;
+            break;
+        }
+    }
+    if (currentPlayer == "yellow") {
+        currentPlayer = "red"
+        document.getElementById('player-counter').style.background = "red"
+    } else {
+        currentPlayer = "yellow"
+        document.getElementById('player-counter').style.background = "yellow"
+    }
+    console.log(gameBoardValues);
 }
 
 function setupBoard() {
@@ -22,20 +57,33 @@ function setupBoard() {
     let row = document.getElementById("row-input").value;
     let column = document.getElementById("column-input").value;
     let gameBoard = document.getElementById("game-board");
+    setupGameBoardValues(row, column);
     for (let i = 0; i < column; i++) {
         let divColumn = document.createElement("div");
+        divColumn.addEventListener("click", function() {
+            placeCounter(i);
+        });
         divColumn.className = "column flex-container";
         divColumn.id = i;
 
         for (let j = 0; j < row; j++) {
             let divRow = document.createElement("div");
             divRow.className = "row";
-            divRow.id = j;
-            divRow.innerText = "[" + j + ", " + i + "]";
+            divRow.id = i + "," + j;
             divColumn.appendChild(divRow);
         }
         gameBoard.appendChild(divColumn);
     }
+}
+
+module = module || {};
+module.exports = {
+    resetBoard: resetBoard,
+    setupGameBoardValues: setupGameBoardValues,
+    placeCounter: placeCounter,
+    setupBoard: setupBoard,
+    getGameBoardValues: getGameBoardValues,
+    setGameBoardValues: setGameBoardValues,
 }
 
 // function setupBoard() {
