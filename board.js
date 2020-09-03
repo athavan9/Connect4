8f76cad048fc2029e6f330b87d2fe376eccf99bd
Athavan Srividiyakaran <athavan@athavans-mbp.mynet>
2020-09-03 10:32:53 +0100

    Changed functionality to JQuery

--- board.js
+++ board.js
@@ -1,7 +1,5 @@
-//setupBoard(row, column);
-
-let gameBoardValues = []
-let currentPlayer = 'red'
+let gameBoardValues = [];
+let currentPlayer = 'red';
 
 function getGameBoardValues() {
     return gameBoardValues;
@@ -12,13 +10,15 @@ function setGameBoardValues(updatedGameBoardValues) {
 }
 
 function resetBoard(row, column) {
-    document.getElementById("game-board").innerHTML = "";
+    //document.getElementById("game-board").innerHTML = "";
+    $("#game-board").html("");
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < column; j++) {
             gameBoardValues[row][column] = null;
         }
     }
-    document.getElementById("player-counter").style.background = "red"
+    $("#player-counter").css("background-color", "red");
+    currentPlayer = "red"
 }
 
 function setupGameBoardValues(row, column) {
@@ -28,7 +28,7 @@ function setupGameBoardValues(row, column) {
         for (let j = 0; j < row; j++) {
             tempArray.push(null);
         }
-        customGameBoardValues.push(tempArray)
+        customGameBoardValues.push(tempArray);
     }
     gameBoardValues = customGameBoardValues;
 }
@@ -36,55 +36,70 @@ function setupGameBoardValues(row, column) {
 function placeCounter(columnIndex) {
     for (let i = gameBoardValues[columnIndex].length - 1; i >= 0; i--) {
         if (gameBoardValues[columnIndex][i] == null) {
-            gameBoardValues[columnIndex][i] = "red";
-            let slot = document.getElementById(columnIndex + "," + i)
-            slot.style.background = currentPlayer;
+            gameBoardValues[columnIndex][i] = currentPlayer;
+            let counterPos = columnIndex + "-" + i
+            $("#" + counterPos).css("background-color", currentPlayer);
+            //document.getElementById(counterPos).style.background = currentPlayer;
             break;
         }
     }
     if (currentPlayer == "yellow") {
         currentPlayer = "red"
-        document.getElementById('player-counter').style.background = "red"
+        $("#player-counter").css("background-color", "red");
     } else {
         currentPlayer = "yellow"
-        document.getElementById('player-counter').style.background = "yellow"
+        $("#player-counter").css("background-color", "yellow");
     }
     console.log(gameBoardValues);
 }
 
 function setupBoard() {
     resetBoard();
-    let row = document.getElementById("row-input").value;
-    let column = document.getElementById("column-input").value;
+    let row = $("#row-input").val();
+    let column = $("#column-input").val();
     let gameBoard = document.getElementById("game-board");
+    //let gameBoard = $("#game-board");
     setupGameBoardValues(row, column);
+
     for (let i = 0; i < column; i++) {
-        let divColumn = document.createElement("div");
-        divColumn.addEventListener("click", function() {
-            placeCounter(i);
-        });
-        divColumn.className = "column flex-container";
-        divColumn.id = i;
+        let divColumn = $("<div></div>");
+        //let divColumn = document.createElement("div");
+        divColumn.click(function() {
+                placeCounter(i);
+            })
+            /*divColumn.addEventListener("click", function() {
+                                        placeCounter(i);
+                                    });*/
+        divColumn.attr("class", "column flex-container");
+        //divColumn.className = "column flex-container";
+        divColumn.attr("id", i);
+        //divColumn.id = i;
 
         for (let j = 0; j < row; j++) {
-            let divRow = document.createElement("div");
-            divRow.className = "row";
-            divRow.id = i + "," + j;
-            divColumn.appendChild(divRow);
+            let divRow = $("<div></div>");
+            //let divRow = document.createElement("div");
+            divRow.attr("class", "row");
+            //divRow.className = "row";
+            divRow.attr("id", i + "-" + j);
+            //divRow.id = i + "-" + j;
+            divColumn.append(divRow);
+            //divColumn.append(divRow);
         }
-        gameBoard.appendChild(divColumn);
+
+        $("#game-board").append(divColumn);
+        //gameBoard.appendChild(divColumn);
     }
 }
 
-module = module || {};
-module.exports = {
+//module = module || {};
+/*module.exports = {
     resetBoard: resetBoard,
     setupGameBoardValues: setupGameBoardValues,
     placeCounter: placeCounter,
     setupBoard: setupBoard,
     getGameBoardValues: getGameBoardValues,
     setGameBoardValues: setGameBoardValues,
-}
+}*/
 
 // function setupBoard() {
 //     resetBoard();
