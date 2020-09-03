47e798f389db8b1024fd564b342f10b6e25071da
Athavan Srividiyakaran <athavan@athavans-mbp.mynet>
2020-09-03 20:42:25 +0100

    Add a working checkColumnWin function

--- board.js
+++ board.js
@@ -2,7 +2,6 @@ let gameBoardValues = [];
 let currentPlayer = 'red';
 
 function resetBoard(row, column) {
-  // document.getElementById("game-board").innerHTML = "";
   $('#game-board').html('');
   for (let i = 0; i < row; i += 1) {
     for (let j = 0; j < column; j += 1) {
@@ -31,7 +30,8 @@ function placeCounter(columnIndex) {
       gameBoardValues[columnIndex][i] = currentPlayer;
       const counterPos = `${columnIndex}-${i}`;
       $(`#${counterPos}`).css('background-color', currentPlayer);
-      // document.getElementById(counterPos).style.background = currentPlayer;
+
+      checkWinner(columnIndex, i);
       break;
     }
   }
@@ -50,35 +50,78 @@ function setupBoard() {
   resetBoard();
   const row = $('#row-input').val();
   const column = $('#column-input').val();
-  // let gameBoard = document.getElementById('game-board');
-  // let gameBoard = $("#game-board");
   setupGameBoardValues(row, column);
 
   for (let i = 0; i < column; i += 1) {
     const divColumn = $('<div></div>');
-    // let divColumn = document.createElement("div");
     divColumn.click(() => {
       placeCounter(i);
     });
     divColumn.attr('class', 'column flex-container');
-    // divColumn.className = "column flex-container";
     divColumn.attr('id', i);
-    // divColumn.id = i;
 
     for (let j = 0; j < row; j += 1) {
       const divRow = $('<div></div>');
-      // let divRow = document.createElement("div");
       divRow.attr('class', 'row');
-      // divRow.className = "row";
       divRow.attr('id', `${i}-${j}`);
-      // divRow.id = i + "-" + j;
       divColumn.append(divRow);
-      // divColumn.append(divRow);
     }
 
     $('#game-board').append(divColumn);
-    // gameBoard.appendChild(divColumn);
+    $('#player-turn-area').attr('style', 'display: block');
   }
+}
+
+function checkColumnWin(columnIndex) {
+  let lossDetected = true;
+  let fourInARowCount = 0;
+  for (let i = gameBoardValues[columnIndex].length - 1; i >= 0; i -= 1) {
+    console.log(i + ". CheckPiece: " + gameBoardValues[columnIndex][i]);
+    if ((i + 1) >= 4 && gameBoardValues[columnIndex][i] !== null) {
+      fourInARowCount = 0;
+      for (let j = 1; j < 4; j += 1) {
+        const nextRowUpPiece = gameBoardValues[columnIndex][i - j];
+        console.log("gameBoardValues [" + columnIndex + "][" + (i - j) + "] - " + nextRowUpPiece);
+        if (gameBoardValues[columnIndex][i] !== nextRowUpPiece || nextRowUpPiece === null) {
+          console.log("Loss Detected");
+          lossDetected = true;
+          break;
+        } else if (gameBoardValues[columnIndex][i] === nextRowUpPiece) {
+          fourInARowCount += 1;
+        }
+      }
+    } else {
+      break;
+    }
+    if (fourInARowCount === 3) {
+      console.log("Win");
+      break;
+    }
+  }
+}
+
+function checkRowWin(rowIndex) {
+  let winDetected = true;
+  for (let i = 0; i < gameBoardValues.length; i += 1) {
+    if (gameBoardValues.length - i >= 4) {
+      for (let j = 1; j < 4; j += 1) {
+        if (gameBoardValues[i][rowIndex] !== gameBoardValues[i + j][rowIndex]) {
+          winDetected = false;
+          break;
+        }
+      }
+    }
+    if (winDetected === true) {
+      console.log("Win");
+      break;
+    }
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+function checkWinner(columnIndex, rowIndex) {
+  checkColumnWin(columnIndex);
+  checkRowWin(rowIndex);
 }
 
 // module = module || {};
