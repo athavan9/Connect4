350331fc9b84c04219bbdc3247e180fca0121b02
Athavan Srividiyakaran <athavan@Athavans-MBP.localdomain>
2020-09-22 15:59:52 +0100

    Adds test for lowest diagonal points

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,9 +1,5 @@
-// const request = require('supertest');
-// const app = require('./server');
-
 const {
   setupGameBoardValuesArray,
-  resetBoardArray,
   updateStateValues,
   changePlayer,
   placeCounter,
@@ -297,6 +293,118 @@ describe('Checks for Row Wins', () => {
     // Assert
     expect(actualWinner).toBe(expectedWinner);
   });
+});
+
+describe('Checks for Right Diagonal Wins', () => {
+  const fiveBySixBoard = [
+    [null, null, null, 'yellow', 'yellow'],
+    [null, null, 'red', 'yellow', 'yellow'],
+    [null, 'yellow', 'yellow', 'red', null],
+    ['red', 'yellow', 'red', null, null],
+    [null, 'red', null, null, null],
+    ['red', null, null, null, null],
+  ];
+
+  const cases = [
+    [3, 1, fiveBySixBoard, 'yellow'], // Yellow Diagonal Win detected from the end (diagonal starting at row 0)
+    [5, 0, fiveBySixBoard, 'red'], // Red Diagonal Win detected from the end (diagonal starting after row 0)
+    [0, 4, fiveBySixBoard, 'yellow'], // Yellow win detected from the start of a diagonal
+    [2, 2, fiveBySixBoard, 'yellow'], // Yellow Win detected from the middle of a diagonal
+    [5, 1, fiveBySixBoard, null], // No Diagonal Win returns null with an empty diagonal
+    [3, 0, fiveBySixBoard, null], // No Diagonal Win returns null with a full diagonal
+  ];
+  test.each(cases)(
+    'When given %s rows and %s columns, it returns an empty initial game board array of that size',
+    (numRows, numColumns, testBoard, expectedWinner) => {
+      // Act
+      const winColour = checkRightDiagonalWin(numRows, numColumns, testBoard);
+      expect(winColour).toBe(expectedWinner);
+    },
+  );
+});
+
+describe('Checks for Left Diagonal Wins', () => {
+  const fiveBySixBoard = [
+    [null, 'yellow', null, null, null],
+    ['red', 'red', 'yellow', null, null],
+    [null, 'red', 'yellow', 'yellow', null],
+    [null, null, 'red', 'red', 'yellow'],
+    [null, null, null, 'red', 'yellow'],
+    [null, null, null, null, 'yellow'],
+  ];
+
+  const cases = [
+    [0, 1, fiveBySixBoard, 'yellow'], // Yellow Diagonal Win detected from the end (diagonal starting at row 0)
+    [3, 4, fiveBySixBoard, 'yellow'], // Yellow win detected from the start of a diagonal
+    [1, 2, fiveBySixBoard, 'yellow'], // Yellow Win detected from the middle of a diagonal
+    [1, 0, fiveBySixBoard, 'red'], // Red Diagonal Win detected from the end (diagonal starting after row 0)
+    [2, 0, fiveBySixBoard, null], // No Diagonal Win returns null with an empty diagonal
+    [1, 1, fiveBySixBoard, null], // No Diagonal Win returns null with a full diagonal
+  ];
+  test.each(cases)(
+    'When given %s rows and %s columns, it returns an empty initial game board array of that size',
+    (numRows, numColumns, testBoard, expectedWinner) => {
+      // Act
+      const winColour = checkLeftDiagonalWin(numRows, numColumns, testBoard);
+      expect(winColour).toBe(expectedWinner);
+    },
+  );
+});
+
+describe('Checks for Lowest Left Diagonal Point', () => {
+  const fiveBySixBoard = [
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+  ];
+
+  const cases = [
+    [0, 4, fiveBySixBoard, [0, 4]],
+    [1, 3, fiveBySixBoard, [0, 4]],
+    [2, 2, fiveBySixBoard, [0, 4]],
+    [0, 3, fiveBySixBoard, [0, 3]],
+    [1, 2, fiveBySixBoard, [0, 3]],
+    [1, 0, fiveBySixBoard, [0, 1]],
+  ];
+  test.each(cases)(
+    'When given %s rows and %s columns, it returns an empty initial game board array of that size',
+    (numRows, numColumns, testBoard, expectedWinner) => {
+      // Act
+      const winColour = getLeftDiagnoalPoint(numRows, numColumns, testBoard);
+      expect(winColour).toStrictEqual(expectedWinner);
+    },
+  );
+});
+
+describe('Checks for Lowest Right Diagonal Point', () => {
+  const fiveBySixBoard = [
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+  ];
+
+  const cases = [
+    [5, 4, fiveBySixBoard, [5, 4]],
+    [4, 3, fiveBySixBoard, [5, 4]],
+    [3, 2, fiveBySixBoard, [5, 4]],
+    [4, 4, fiveBySixBoard, [4, 4]],
+    [3, 3, fiveBySixBoard, [4, 4]],
+    [4, 2, fiveBySixBoard, [5, 3]],
+  ];
+  test.each(cases)(
+    'When given %s rows and %s columns, it returns an empty initial game board array of that size',
+    (numRows, numColumns, testBoard, expectedWinner) => {
+      // Act
+      const winColour = getRightDiagnoalPoint(numRows, numColumns, testBoard);
+      expect(winColour).toStrictEqual(expectedWinner);
+    },
+  );
 });
 
 describe('Check for a Winner', () => {
